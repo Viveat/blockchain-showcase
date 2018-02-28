c66c73c76f2ad4f9bad0b795bf4c04d8fab1ea25
Guido Casiraghi <guido.casiraghi@gmail.com>
2018-02-28 12:41:53 +0100

    Work in progress deploy su testnet

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // Import libraries
 var Web3 = require('web3'),
 	contract = require("truffle-contract"),
-	path = require('path')
+	HDWalletProvider = require("truffle-hdwallet-provider"),
+	path = require('path'),
 	ViveatJSON = require(path.join(__dirname, 'build/contracts/Viveat.json'));
 
 // Setup RPC connection   
-var provider = new Web3.providers.HttpProvider("http://localhost:7545");
-
+// var provider = new Web3.providers.HttpProvider("http://localhost:7545");
+mnemonic = 'hazard width agent oven vapor pink surge endorse urban slam town state';
+var provider = new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/5rV6AOWj8UarOdOp4e0f", 4);
+accountAddress = '0x08a3fbfFd216cb2D255f413D499AcdC7A366Ca5A'
 
 // Read JSON and attach RPC connection (Provider)
 var Viveat = contract(ViveatJSON);
@@ -19,15 +22,15 @@ Viveat.deployed()
 	.then(function(instance) {
 		contactInstance = instance;
 		// return instance.verifyProduct.call(12345)
-		return instance.addProduct('bellaborsa', 'guidobrand', 10000, 12345, {from: '0x821aEa9a577a9b44299B9c15c88cf3087F3b5544'});
+		return instance.addProduct('bellaborsa', 'guidobrand', 10000, 12345, {from: accountAddress});
 	})
 	.then(function(result) {
-		if (result && result.receipt.status == 1) {
-			console.log('Product added correctly');
-		}
+		// if (result && result.receipt.status == 1) {
+		// 	console.log('Product added correctly');
+		// }
 	})
 	.then(function(result) {
-		return contactInstance.verifyProduct.call(12345, {from: '0x821aEa9a577a9b44299B9c15c88cf3087F3b5544'});
+		return contactInstance.verifyProduct.call(12345, {from: accountAddress});
 	})
 	.then(function(result) {
 		console.log(result);
--- truffle.js
+++ truffle.js
@@ -1,12 +1,25 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+mnemonic = 'hazard width agent oven vapor pink surge endorse urban slam town state';
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
+
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545, // ganache...
       // port: 8545, // testrpc...
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/5rV6AOWj8UarOdOp4e0f")
+      },
+      network_id: 4,
+      // gas: 4700000,
+      // gasPrice: 6000000000
     }
   }
 };
